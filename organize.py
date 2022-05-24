dbdfa17a50c6338d4a3c84ff906da05de6ca2a51
CohentheCat <tyjameshancock@gmail.com>
2022-05-24 13:58:24 +0930

    Added the os.mkdir method so that the script works

--- organize.py
+++ organize.py
@@ -1,12 +1,10 @@
-from genericpath import exists
-from msilib.schema import Error
 import os
 import collections
 from pprint import pprint
 
 #Selects the folder you want to sort
-
-TARGET_PATH = os.path.join(os.path.expanduser("~"), 'Downloads')
+folder_to_sort = 'Downloads'
+TARGET_PATH = os.path.join(os.path.expanduser("~"), folder_to_sort)
 
 #Will make folders for each file type in the given directory if one does not exist.
 file_mappings = collections.defaultdict()
@@ -21,16 +19,19 @@ for folder_name, folder_items in file_mappings.items():
     folder_path = os.path.join(TARGET_PATH, folder_name)
     if not os.path.exists(folder_path):
         pprint(f"Need to make a {folder_path} folder!")
+        os.mkdir(folder_path)
     
     for folder_item in folder_items:
         source = os.path.join(TARGET_PATH, folder_item)
         destination = os.path.join(folder_path, folder_item)
         pprint(f"Moving: {source} to {destination}")
         try:
-            os.rename(source, destination)
+            if source != destination:
+                os.rename(source, destination)
 
         except FileExistsError:
             pprint("Sorry a file of this name exists in this folder")
         
         except PermissionError:
             pprint("Hang on a sec...")
+
